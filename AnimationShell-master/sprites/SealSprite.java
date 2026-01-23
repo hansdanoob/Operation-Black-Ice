1dd6d9502b3e13efbe0e6ba06c0f8ea52b1d18a9
hansdanoob <josiahandrews9@gmail.com>
2026-01-23 13:07:09 -0700

    comment fix

--- AnimationShell-master/sprites/SealSprite.java
+++ AnimationShell-master/sprites/SealSprite.java
@@ -299,7 +299,7 @@ public class SealSprite implements DisplayableSprite {
 					directionsMoving.add(Direction.EAST);
 				}
 			} else {
-			collsionCompensationToY = velocityY / (velocityX / 2); // half of x velocity (currently going into a wall) is transferred
+			collsionCompensationToY = velocityY / (velocityX / 2); // part of x velocity (currently going into a wall) is transferred
 			}
 			
 		}
@@ -317,7 +317,7 @@ public class SealSprite implements DisplayableSprite {
 					directionsMoving.add(Direction.NORTH);
 				}
 			} else {
-				collsionCompensationToX = velocityX / (velocityY / 2); // part of x velocity (currently going into a wall) is transferred
+				collsionCompensationToX = velocityX / (velocityY / 2);
 			}
 		}
 
